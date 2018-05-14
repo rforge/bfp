ca1244e5f4c9bdfe06046c4394ce3a78dfa2b366
danielinteract <danielinteract@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-05-14 10:32:17 +0000

    use C++11 consistently, which allows to have exception for throw in a destructor in newmat5.cpp
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bfp@149 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bfp/src/newmat/newmat.h
+++ pkg/bfp/src/newmat/newmat.h
@@ -1833,7 +1833,7 @@ class MatrixInput
 public:
    MatrixInput(const MatrixInput& mi) : n(mi.n), r(mi.r) {}
    MatrixInput(int nx, Real* rx) : n(nx), r(rx) {}
-   ~MatrixInput();
+   ~MatrixInput() noexcept(false);
    MatrixInput operator<<(double);
    MatrixInput operator<<(float);
    MatrixInput operator<<(int f);
